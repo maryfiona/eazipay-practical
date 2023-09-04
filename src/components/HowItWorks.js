50fab9f272e5547861e1c538b6e2da6f54439547
maryfiona <aguchidimmamaryfiona@gmail.com>
2023-09-04 22:20:39 +0100

    done1

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -1,9 +1,37 @@
 import './HowItWorks.css'
 // import circle from '../assets/circle.svg'
+import easipay from '../assets/easipay.svg'
 // import  phone from '../assets/phone.svg'
 function HowItWorks () {
     return(
         <>
+           <div>
+             <div>
+                    <ul className="flex-no-wrap relative flex w-full items-center justify-between  py-2 shadow-md shadow-black/5 lg:flex-wrap 
+                    lg:justify-start lg:py-4">
+                        <div className='flex'>
+
+                       
+                        <img  src={easipay} alt="easipay" />
+                        <li >
+                        <a  href="/">Individual</a>
+                        </li>
+                        <li>
+                        <a href="/">Business</a>
+                        </li>
+                        <li>
+                        <a href="/">Pricing</a>
+                        </li>
+                        <li>
+                        <a href="/">Set your payroll</a>
+                        </li>
+                        <button className="bg-white  border-2 border-gree900" >Login </button>
+                       <button className="bg-green-800  rounded-full text-white " >Register </button>
+                    </div>
+                    </ul>
+
+                </div>
+        </div> 
         </>
                 
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,18 +1,18 @@
 import Navbar from '../components/Navbar'
-import LandingPage from '../components/LandingPage'
- import Individual from '../components/Individual'
- // import HowItWorks from '../components/HowItWorks';
- import Advert from '../components/Advert'
- import Demo from '../components/Demo'
+// import LandingPage from '../components/LandingPage'
+//  import Individual from '../components/Individual'
+ //import HowItWorks from '../components/HowItWorks';
+//  import Advert from '../components/Advert'
+//  import Demo from '../components/Demo'
 function HomePage() {
     return (
       <>
       <Navbar />
-         <LandingPage /> 
+         {/* <LandingPage /> 
        <Individual />
-        {/* <HowItWorks /> */}
+        <HowItWorks />
      <Advert /> 
-     <Demo />
+     <Demo /> */}
       </>
      
     );
